442fdf081603651cf1c9e5a6c58a8dcec67e891c
jemrich18 <jemrich18@gmail.com>
2026-03-05 08:36:29 -0600

    add root URL redirect

--- hunt/urls.py
+++ hunt/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views 
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('dashboard'), name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='hunt/login.html'), name='login'),
